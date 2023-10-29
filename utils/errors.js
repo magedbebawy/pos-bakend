77611f3373cd92cc02781bfff3d816b29c0fc6e6
bebawymaged <102050541+bebawymaged@users.noreply.github.com>
2023-10-29 13:14:46 -0700

    user routes initial setup

---
+++ utils/errors.js
@@ -0,0 +1,28 @@
+class Errors {
+    success(message, data) {
+        return {
+            message,
+            statusCode: 200,
+            error: null,
+            data
+        }
+    }
+
+    serverError(error) {
+        return {
+            message: 'Internal server error',
+            statusCode: 500,
+            error
+        }
+    }
+
+    validationError(error) {
+        return {
+            message: 'Validation error',
+            statusCode: 400,
+            error
+        }
+    }
+}
+
+module.exports = Errors;
